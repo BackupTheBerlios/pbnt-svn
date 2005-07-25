a7ac062a59a59f0cfe0ff4d88701c212a5c98087
elliot <elliot@ea4317b7-edfa-0310-9160-bc06dddd0196>
2005-07-25 08:39:03 +0000

    slight changes to JunctionTreeEngine but still part of working

--- trunk/working/Inference/JunctionTreeEngine.py
+++ trunk/working/Inference/JunctionTreeEngine.py
@@ -1,32 +1,16 @@
 class JunctionTreeEngine(InferenceEngine):
 
 	def __init__ (self,bnet):
-		moralGraph = ConstructMoralGraph(bnet)
-		triGraph = 
-		joinTree = BuildJoinTree(triGraph)
-
-		self.bnet = bnet
-		self.evidence = array()
-		self.moralGraph = ConstructMoralGraph(self.bnet)
-		self.triGraph = Triangulate(self.moralGraph)
-		self.joinTree = BuildJoinTree(self.triGraph)
-					      
-
-	def ConstructMoralGraph(bnet):
-		Gm = copy(bnet.table())
-		#make undirected
-		for i in range(shape(Gu))[0]:
-			connections =  find(Gu[i,:]==1)
-			for j in connections:
-				take(Gu,j,i) = 0
-		#connect parents
-		for i in range(shape(Gu)[0]:
-			parents = bnet.parents(i)
-			for k in [0:parents.length()-1]:
-				for l in [k+1:parents.length()-1]:
-					take(Gu,parents[k],parents[l]) = 1
+		InferenceEngine(self, bnet)
+		joinTree = BuildJoinTree(bnet)
 	
-		return Gu
+	def BuildJoinTree ( self, bnet ):
+		#make moral graph by making all of the parents children,
+		#basically create an undirected graph
+		Gm = bnet.graph.copy()
+		Gm += transpose(Gm)
+		
+		
 	
 	def TriangulateGraph(moralG):
 		TgP = copy(moralG)
