8a9a85d7a231fb303826bc917e9c7b28cf2a80e7
elliot <elliot@ea4317b7-edfa-0310-9160-bc06dddd0196>
2005-08-11 08:27:02 +0000

    started implementing DBNs, starting with inference.  Also started relocating code, collecting into single files, eg all node classes are now in one file Node.py.  Haven't completely finished reorganizing modules and still have to physically reorganize the files themselves.  Files that were removed, are now in there respective parent module.

--- trunk/General/utilities.py
+++ trunk/General/utilities.py
@@ -73,6 +73,33 @@ def updateCounts(nodes, counts, data):
 		indices = data[concatenate((node.parentIndex, array([node.index])))]
 		fIndex = flatIndex(indices, count.shape)
 		count.flat[fIndex] += 1
+
+class SequenceGenerator:
+	
+	def __init__( self, iterObjs ):
+		self.stop = array(iterObjs) - 1
+		#assuming here that the start of each dimension is zero
+		self.value = zeros(size( iterObjs ))
+		if len( self.value ) > 0:
+			self.value[0] -= 1
+		self.start = self.value.copy()
+	
+	def __iter__( self ):
+		return self
+	
+	def next( self ):
+		if alltrue( self.value == self.stop ):
+			self.value = self.start.copy()
+			raise StopIteration
+		
+		for i in range(size( self.stop )):
+			if self.value[i] == self.stop[i]:
+				self.value[i] = 0
+			else:
+				self.value[i] += 1
+				break
+		
+		return self.value
 	
 
 
