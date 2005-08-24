87bafd03b41b49b975f3f8cd7e3a6845934e033f
elliot <elliot@ea4317b7-edfa-0310-9160-bc06dddd0196>
2005-08-24 08:55:02 +0000

    little more reorg, wrote unit test for topo sort in graph using PyUnit

---
+++ trunk/dist/tests/GraphUnitTest.py
@@ -0,0 +1,55 @@
+# Builtin Python Libraries
+import unittest
+# Major Packages
+import numarray
+# Library specific modules
+import Graph
+import Node
+
+class TopoSortTestCase(unittest.TestCase):
+    def setUp(self):
+        a = Node.DirectedNode(1)
+        b = Node.DirectedNode(2)
+        c = Node.DirectedNode(3)
+        d = Node.DirectedNode(4)
+        e = Node.DirectedNode(5)
+        
+        a.add_child(b)
+        a.add_child(c)
+        b.add_parent(a)
+        c.add_parent(a)
+        d.add_child(b)
+        b.add_parent(d)
+        e.add_child(a)
+        a.add_parent(e)
+        
+        self.nodes = [a,b,c,d,e]
+    
+    def testBasicSetIndex(self):
+        """ Test that indices are correct relative to each other, using very basic network structure
+        """
+        self.graph = Graph.DAG(self.nodes)
+        assert(self.nodes[0].index > self.nodes[4].index and \
+               self.nodes[1].index > self.nodes[0].index and \
+               self.nodes[1].index > self.nodes[3].index and \
+               self.nodes[2].index > self.nodes[0].index), \
+              "Indexes were not set properly in DAG.topological_sort()"
+    
+    def testAllIndexSet(self):
+        """ Test that all indices are >= 0
+        """
+        self.graph = Graph.DAG(self.nodes)
+        for node in self.nodes:
+            assert(node.index >= 0), "Index was less than 0"
+    
+    def testRaiseCyclicException(self):
+        """ Test that the cyclic graph raises an exception.
+        """
+        self.nodes[0].add_child(self.nodes[4])
+        self.nodes[4].add_parent(self.nodes[0])
+        self.assertRaises(AssertionError, Graph.DAG, self.nodes)
+
+def suite():
+    return unittest.makeSuite(TopoSortTestCase, 'test')
+#runner = unittest.TextTestRunner()
+#runner.run(suite)
--- trunk/working/CliqueNode.py
+++
@@ -1,8 +0,0 @@
-from BayesNode import *
-
-class CliqueNode( BayesNode ):
-        
-        def __init__( self, node ):
-                self = node
-                self.clique = -1
-        
